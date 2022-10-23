bf49198cd4de3301480dbfa206b6c17eded5bb5c
Tomasz <TSzydlowski@protonmail.com>
2022-10-23 09:17:11 +0200

    clear data settings page

--- src/view/pages/SettingsPage.tsx
+++ src/view/pages/SettingsPage.tsx
@@ -1,11 +1,21 @@
-﻿import React, { FC } from 'react'
+﻿import { Button } from 'antd'
+import React, { FC } from 'react'
 
+import { AZURE_DEV_OPS_TOKEN, CLOCKIFY_KEY } from '../../core/consts/consts'
 import { BaseLayout } from '../components/layouts/BaseLayout'
 
 export const SettingsPage: FC = () => {
+    const clearData = () => {
+        localStorage.removeItem(CLOCKIFY_KEY)
+        localStorage.removeItem(AZURE_DEV_OPS_TOKEN)
+        window.location.reload()
+    }
+
     return (
         <BaseLayout>
-            <div>Settings PAGE</div>
+            <Button type={'primary'} onClick={clearData}>
+                Clear data
+            </Button>
         </BaseLayout>
     )
 }
