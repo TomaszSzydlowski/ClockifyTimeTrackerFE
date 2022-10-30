22513dd87f5cfb622bb31a17b34563e6e5caa5ad
Tomasz <TSzydlowski@protonmail.com>
2022-10-30 21:32:29 +0100

    search data prototype

--- src/view/pages/SearchPage.tsx
+++ src/view/pages/SearchPage.tsx
@@ -1,11 +1,56 @@
-﻿import React, { FC } from 'react'
+﻿import { Collapse } from 'antd'
+import { Input } from 'antd'
+import { SearchBar } from 'antd-mobile'
+import React, { FC, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Y } from 'vitest/dist/global-732f9b14'
 
+import { ProjectView } from '../../core/types/Project'
+import { projectsClockifySelectors } from '../../store/features/clockify/projects/selectors'
+import { yoursProjectsSelectors } from '../../store/features/clockify/yours-projects/selectors'
 import { BaseLayout } from '../components/layouts/BaseLayout'
 
+const { Panel } = Collapse
+const { Search } = Input
+
 export const SearchPage: FC = () => {
+    const projects = useSelector(projectsClockifySelectors.getProjects)
+    const yoursProjects = useSelector(yoursProjectsSelectors.getYoursProjects)
+    const [yoursProjectsView, setYoursProjectsView] = useState<ProjectView[]>([])
+
+    useEffect(() => {
+        if (
+            !projects ||
+            projects.length === 0 ||
+            !yoursProjects ||
+            yoursProjects.length === 0
+        )
+            return
+
+        const newYoursProjectsView = projects.filter((project) =>
+            yoursProjects.includes(project.id),
+        )
+        setYoursProjectsView(newYoursProjectsView)
+    }, [projects, yoursProjects])
+
+    const onSearch = (value: string) => console.log(value)
+
     return (
         <BaseLayout>
-            <div>Search PAGE</div>
+            <div className="search_page">
+                <Search placeholder="input search text" allowClear onSearch={onSearch} />
+                <div className="search_page_collapse">
+                    <Collapse>
+                        {yoursProjectsView.map((projectView, index) => (
+                            <Panel key={index} header={projectView.name}>
+                                {projectView.tasks.map((task, index) => (
+                                    <p key={index}>{task.name}</p>
+                                ))}
+                            </Panel>
+                        ))}
+                    </Collapse>
+                </div>
+            </div>
         </BaseLayout>
     )
 }
