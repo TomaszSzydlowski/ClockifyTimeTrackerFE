27ca52d60a4faf57bb9ac297e7a5e5319ba201cc
Tomasz <TSzydlowski@protonmail.com>
2022-10-31 21:33:51 +0100

    added project list by ant design mobile checklist

---
+++ src/view/components/ProjectList.tsx
@@ -0,0 +1,65 @@
+﻿import { Button, CheckList, Popup, SearchBar, Space } from 'antd-mobile'
+import React, { FC, useMemo, useState } from 'react'
+
+const items = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+export const ProjectList: FC = () => {
+    const [visible, setVisible] = useState(true)
+    const [selected, setSelected] = useState('A')
+    const [searchText, setSearchText] = useState('')
+    const filteredItems = useMemo(() => {
+        if (searchText) {
+            return items.filter((item) => item.includes(searchText))
+        } else {
+            return items
+        }
+    }, [items, searchText])
+
+    return (
+        <>
+            <Space align="center">
+                <Button
+                    onClick={() => {
+                        setVisible(true)
+                    }}
+                >
+                    Select Yours Projects
+                </Button>
+                <div>Selected: {selected}</div>
+            </Space>
+            <Popup
+                visible={visible}
+                onMaskClick={() => {
+                    setVisible(false)
+                }}
+                destroyOnClose
+            >
+                <div className={'styles.searchBarContainer'}>
+                    <SearchBar
+                        placeholder="输入文字过滤选项"
+                        value={searchText}
+                        onChange={(v) => {
+                            setSearchText(v)
+                        }}
+                    />
+                </div>
+                <div className={'styles.checkListContainer'}>
+                    <CheckList
+                        className={'styles.myCheckList'}
+                        defaultValue={selected ? [selected] : []}
+                        onChange={(val) => {
+                            setSelected(val[0])
+                            setVisible(false)
+                        }}
+                    >
+                        {filteredItems.map((item) => (
+                            <CheckList.Item key={item} value={item}>
+                                {item}
+                            </CheckList.Item>
+                        ))}
+                    </CheckList>
+                </div>
+            </Popup>
+        </>
+    )
+}
--- src/view/pages/SettingsPage.tsx
+++ src/view/pages/SettingsPage.tsx
@@ -1,26 +1,35 @@
-﻿import { Button } from 'antd'
+﻿import { Button, Select } from 'antd'
 import React, { FC } from 'react'
 
-import {
-    AZURE_DEV_OPS_TOKEN,
-    CLOCKIFY_KEY,
-    YOURS_PROJECTS,
-} from '../../core/consts/consts'
+import { AZURE_DEV_OPS_TOKEN, CLOCKIFY_KEY } from '../../core/consts/consts'
 import { BaseLayout } from '../components/layouts/BaseLayout'
+import { ProjectList } from '../components/ProjectList'
 
+const { Option } = Select
+
+const children: React.ReactNode[] = []
+for (let i = 10; i < 36; i++) {
+    children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>)
+}
+
+const handleChange = (value: string[]) => {
+    console.log(`selected ${value}`)
+}
 export const SettingsPage: FC = () => {
     const clearData = () => {
         localStorage.removeItem(CLOCKIFY_KEY)
         localStorage.removeItem(AZURE_DEV_OPS_TOKEN)
-        localStorage.removeItem(YOURS_PROJECTS)
         window.location.reload()
     }
 
     return (
         <BaseLayout>
-            <Button type={'primary'} onClick={clearData}>
-                Clear data
-            </Button>
+            <ProjectList />
+            <div className="settings_page__box">
+                <Button type={'primary'} danger onClick={clearData}>
+                    Clear data
+                </Button>
+            </div>
         </BaseLayout>
     )
 }
