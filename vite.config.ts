7fc1fce995c58daa9f6a6e2949eed3e8935a142b
Tomasz <TSzydlowski@protonmail.com>
2023-03-06 20:28:38 +0100

    Revert "PWA - vitep"
    This reverts commit 5fd1ec0416f40ef92434c27885188b069af998eb.

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,10 @@
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        basicSsl(),
-        VitePWA({
-            manifest: {
-                name: 'Clockify Time Tracker',
-                short_name: 'Clockwise',
-                start_url: '/',
-                display: 'standalone',
-                background_color: '#ffffff',
-                lang: 'en',
-                scope: '/',
-                icons: [
-                    {
-                        src: '/android-chrome-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                        purpose: 'any maskable',
-                    },
-                    {
-                        src: '/android-chrome-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any maskable',
-                    },
-                ],
-                theme_color: '#ffffff',
-            },
-        }),
-    ],
+    plugins: [react(), basicSsl()],
     server: {
         port: 3000,
         https: true,
